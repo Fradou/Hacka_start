e3330e842c46b1ffed79082b27e30ce8c9680400
Alexandre FRADET <a_fradet@yahoo.fr>
2016-12-09 17:25:30 +0100

    EasyAdmin basic config + create demand entity

--- src/TransBundle/Controller/MainController.php
+++ src/TransBundle/Controller/MainController.php
@@ -8,35 +8,35 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TransBundle:Main:index.html.twig', array(
+        return $this->render('Main/index.html.twig', array(
             // ...
         ));
     }
 
     public function demandeAction()
     {
-        return $this->render('TransBundle:Main:demande.html.twig', array(
+        return $this->render('Main/demande.html.twig', array(
             // ...
         ));
     }
 
     public function offerlistAction()
     {
-        return $this->render('TransBundle:Main:offerlist.html.twig', array(
+        return $this->render('Main/offerlist.html.twig', array(
             // ...
         ));
     }
 
     public function comAction()
     {
-        return $this->render('TransBundle:Main:com.html.twig', array(
+        return $this->render('Main/com.html.twig', array(
             // ...
         ));
     }
 
     public function ratingAction()
     {
-        return $this->render('TransBundle:Main:rating.html.twig', array(
+        return $this->render('Main/rating.html.twig', array(
             // ...
         ));
     }
