18f5e3c878c15658299037667fc3c7f818665eee
Alexandre FRADET <a_fradet@yahoo.fr>
2016-12-11 13:57:33 +0100

    Modify translator query. Start styling list

--- src/TransBundle/Form/RegistrationType.php
+++ src/TransBundle/Form/RegistrationType.php
@@ -10,13 +10,17 @@ namespace TransBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Locale\Locale;
+
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('native_language')->add('first_name')->add('last_name')->add('translator')->add('paypal_account')->add('nationality')
+        $builder->add('native_language', LanguageType::class, array ('data' => Locale::getDefault(),))
+            ->add('first_name')->add('last_name')->add('translator')->add('paypal_account')->add('nationality')
         ->add('profile_picture', FileType::class);
     }
 
--- src/TransBundle/Repository/UserRepository.php
+++ src/TransBundle/Repository/UserRepository.php
@@ -9,20 +9,23 @@
 namespace TransBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use TransBundle\Entity\Demand;
 
 
-class MainRepository extends EntityRepository
+class UserRepository extends EntityRepository
 {
     public function findTranslator($demand){
 
         /** On met les % pour pouvoir utiliser "like" et ainsi utiliser de la wildcard.
          * Si pas de % le like se comporterait comme un simple where */
 
+        $searching = $demand;
         $qb= $this->createQueryBuilder('u')
             -> select(array('u'))
-            /**   :country le ":" indique que country est un paramter */
-            ->where('u.nativeLanguage = needed' OR 'u.nativeLanguage = spoken' )
-            ->setParameters(array('needed'=> $demand->languageNeeded, 'spoken' => $demand->languageSpoken))
+            /**   :country le ":" indique que country est un parameter */
+            ->where('u.native_language = :needed OR u.native_language = :spoken' )
+            //  OR u.nativeLanguage = :spoken
+            ->setParameters(array('needed' => $searching->getLanguageSpoken(),'spoken' => $searching->getLanguageNeeded() ))
             ->getQuery();
 
    //         ->where('j.domain = :domain')
